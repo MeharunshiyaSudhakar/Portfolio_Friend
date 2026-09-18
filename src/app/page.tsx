cb887aa2a02ae7045dd20daa9fb0186625502291
Meharunshiya <meharunshiyas26@gmail.com>
2026-09-18 20:16:55 +0530

    Prepared portfolio for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { FaJava, FaLinkedin } from "react-icons/fa";
 
 const LINKEDIN_URL = "https://www.linkedin.com/in/shanmuga-ramesh-664739231/";
 const MAILTO_LINK = "mailto:shanmugaramesh28@gmail.com?subject=Portfolio%20Inquiry&body=Hello%20Shanmuga%2C%0A%0AI%20came%20across%20your%20portfolio%20and%20would%20like%20to%20connect%20with%20you.%0A%0ARegards%2C";
-const BASE_PATH = "/Portfolio_Friend";
+const BASE_PATH = "";
 
 // CUSTOM CURSOR COMPONENT
 const CustomCursor = () => {
